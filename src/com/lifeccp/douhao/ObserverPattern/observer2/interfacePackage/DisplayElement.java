c1cb4a483d3a1d25e9bd561aae2b9e5dce5d6d72
Dou Hao <douhao@wingspan.cn>
2018-06-28 15:49:22 +0800

    增加 观察者模式

---
+++ src/com/lifeccp/douhao/ObserverPattern/observer2/interfacePackage/DisplayElement.java
@@ -0,0 +1,8 @@
+package com.lifeccp.douhao.ObserverPattern.observer2.interfacePackage;
+
+/**
+ * Created by sks on 2018/6/25.
+ */
+public interface DisplayElement {
+    public void display() ;
+}
